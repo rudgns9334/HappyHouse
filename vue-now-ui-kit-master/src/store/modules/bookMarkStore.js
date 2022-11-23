7fe978ad8e73b4cc2b97722a7ffa486fe8d52032
indl1670 <indl1670@gmail.com>
2022-11-23 17:59:15 +0900

    WishList

--- vue-now-ui-kit-master/src/store/modules/bookMarkStore.js
+++ vue-now-ui-kit-master/src/store/modules/bookMarkStore.js
@@ -5,70 +5,75 @@ import VueAlertify from "vue-alertify";
 Vue.use(VueAlertify);
 
 export default {
-    namespaced: true,
-    state: {
-        list:[],
-        selectedIdx: 0,
+  namespaced: true,
+  state: {
+    list: [],
+    selectedIdx: 0,
+  },
+  mutations: {
+    SET_BOOKMARK_LIST(state, list) {
+      state.list = list;
     },
-    mutations: {
-        SET_BOOKMARK_LIST(state, list){
-            state.list = list;
+    SET_SELECTED_IDX(state, idx) {
+      state.selectedIdx = idx;
+    },
+  },
+  getters: {
+    getWishList: function (state) {
+      return state.list;
+    },
+  },
+  actions: {
+    bookMarkList({commit}, userSeq) {
+      bookMarkAxios.bookMarkList(
+        `/${userSeq}`,
+        ({data}) => {
+          if (data.result == 1) {
+            console.log("북마크목록불러오기 성공");
+            commit("SET_BOOKMARK_LIST", data.list);
+          }
         },
-        SET_SELECTED_IDX(state, idx){
-            state.selectedIdx = idx;
+        error => {
+          console.log("불러오기 실패");
+          console.log(error);
         },
+      );
     },
-    actions: {
-        bookMarkList({commit}, userSeq){
-            bookMarkAxios.bookMarkList(
-            `/${userSeq}`,
-            ({data}) => {
-                if(data.result == 1){
-                    console.log("북마크목록불러오기 성공");
-                    commit("SET_BOOKMARK_LIST", data.list);
-                }
-            },
-            (error) => {
-                console.log("불러오기 실패");
-                console.log(error);
-            }
-        )
-       },
-       bookMarkInsert(body){
-        bookMarkAxios.bookMarkInsert(
-            body,
-            ({data}) => {
-                if(data.result == 1){
-                    console.log("관심지역 등록 성공");
-                }
-            },
-            (error) => {
-                console.log("등록 실패");
-                console.log(error);
-            }
-        )
-       },
-       bookMarkDelete(id){
-        bookMarkAxios.bookMarkDelete(
-            `/${id}`,
-            ({data}) => {
-                if(data.result == 1){
-                    console.log("관심지역 삭제 성공");
-                }
-            },
-            (error) => {
-                console.log("삭제 실패");
-                console.log(error);
-            }
-        )
-       },
+    bookMarkInsert(body) {
+      bookMarkAxios.bookMarkInsert(
+        body,
+        ({data}) => {
+          if (data.result == 1) {
+            console.log("관심지역 등록 성공");
+          }
+        },
+        error => {
+          console.log("등록 실패");
+          console.log(error);
+        },
+      );
     },
-    getters: {
-        getSelectedIdx(state){
-        return state.selectedIdx;
-       },
-       getList(state){
-        return state.list;
-       }
-    }
+    bookMarkDelete(id) {
+      bookMarkAxios.bookMarkDelete(
+        `/${id}`,
+        ({data}) => {
+          if (data.result == 1) {
+            console.log("관심지역 삭제 성공");
+          }
+        },
+        error => {
+          console.log("삭제 실패");
+          console.log(error);
+        },
+      );
+    },
+  },
+  getters: {
+    getSelectedIdx(state) {
+      return state.selectedIdx;
+    },
+    getWishList(state) {
+      return state.list;
+    },
+  },
 };
