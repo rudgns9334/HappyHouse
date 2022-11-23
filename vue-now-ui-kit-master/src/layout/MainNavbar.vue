76dfc269a547fc4d95e48c7efae57ba006f235f8
rudgns9334 <rudgns8285@naver.com>
2022-11-23 17:58:16 +0900

    알림판띄우기

--- vue-now-ui-kit-master/src/layout/MainNavbar.vue
+++ vue-now-ui-kit-master/src/layout/MainNavbar.vue
@@ -1,68 +1,84 @@
 <template>
-  <navbar
-    position="fixed"
-    type="primary"
-    :transparent="transparent"
-    :color-on-scroll="colorOnScroll"
-    menu-classes="ml-auto"
-  >
-    <template>
-      <router-link v-popover:popover1 class="navbar-brand" to="/">
-        <img src="img/main-logo.png" alt="" style="width: 100px" />
-      </router-link>
-      <el-popover ref="popover1" popper-class="popover" placement="bottom" width="200" trigger="hover">
-        <div class="popover-body">Designed by Invision. Coded by Creative Tim</div>
-      </el-popover>
-      <div
-        id="header"
-        v-show="isLogin"
-        style="display: inline-block; padding-top: 13px; padding-left: 10px; color: #fff"
-      >
-        Hello, {{ user.userName }}
-      </div>
-    </template>
-    <template slot="navbar-menu">
-      <li class="nav-item" v-show="!isLogin">
-        <router-link class="nav-link" to="/login">
-          <p>Login</p>
+  <div>
+    <navbar
+      position="fixed"
+      type="primary"
+      :transparent="transparent"
+      :color-on-scroll="colorOnScroll"
+      menu-classes="ml-auto"
+    >
+      <template>
+        <router-link v-popover:popover1 class="navbar-brand" to="/">
+          <img src="img/main-logo.png" alt="" style="width: 100px" />
         </router-link>
-      </li>
-      <li class="nav-item" v-show="isLogin">
-        <a class="nav-link" @click="logout">
-          <p>Logout</p>
-        </a>
-      </li>
-      <li class="nav-item" v-show="!isLogin">
-        <router-link class="nav-link" to="/register">
-          <p>Register</p>
-        </router-link>
-      </li>
-      <li class="nav-item" v-show="isLogin">
-        <router-link class="nav-link" to="/profile">
-          <p>MyPage</p>
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/notice">
-          <p>Notice</p>
-        </router-link>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://www.creative-tim.com/product/vue-now-ui-kit" target="_blank">
-          <p>Event</p>
-        </a>
-      </li>
-      <li class="nav-item" v-show="isLogin">
-        <span class="material-symbols-outlined" style="padding-top: 7px; color: #fff"> circle_notifications </span>
-      </li>
-    </template>
-  </navbar>
+        <el-popover
+          ref="popover1"
+          popper-class="popover"
+          placement="bottom"
+          width="200"
+          trigger="hover"
+        >
+          <div class="popover-body">Designed by Invision. Coded by Creative Tim</div>
+        </el-popover>
+        <div
+          id="header"
+          v-show="isLogin"
+          style="display: inline-block; padding-top: 13px; padding-left: 10px; color: #fff"
+        >
+          Hello, {{ user.userName }}
+        </div>
+      </template>
+      <template slot="navbar-menu">
+        <li class="nav-item" v-show="!isLogin">
+          <router-link class="nav-link" to="/login">
+            <p>Login</p>
+          </router-link>
+        </li>
+        <li class="nav-item" v-show="isLogin">
+          <a class="nav-link" @click="logout">
+            <p>Logout</p>
+          </a>
+        </li>
+        <li class="nav-item" v-show="!isLogin">
+          <router-link class="nav-link" to="/register">
+            <p>Register</p>
+          </router-link>
+        </li>
+        <li class="nav-item" v-show="isLogin">
+          <router-link class="nav-link" to="/profile">
+            <p>MyPage</p>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/notice">
+            <p>Notice</p>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            href="https://www.creative-tim.com/product/vue-now-ui-kit"
+            target="_blank"
+          >
+            <p>Event</p>
+          </a>
+        </li>
+        <li class="nav-item" v-show="isLogin">
+          <span class="material-symbols-outlined" style="padding-top: 7px; color: #fff">
+            circle_notifications
+          </span>
+        </li>
+      </template>
+    </navbar>
+    <alerm></alerm>
+  </div>
 </template>
 
 <script>
-import {Navbar} from "@/components";
-import {Popover} from "element-ui";
-import {mapActions, mapState} from "vuex";
+import { Navbar } from "@/components";
+import { Popover } from "element-ui";
+import { mapActions, mapState } from "vuex";
+import Alerm from "../pages/components/Alerm.vue";
 
 const userStore = "userStore";
 
@@ -74,6 +90,7 @@ export default {
   },
   components: {
     Navbar,
+    Alerm,
     [Popover.name]: Popover,
   },
   computed: {
--- vue-now-ui-kit-master/src/pages/components/Alerm.vue
+++ vue-now-ui-kit-master/src/pages/components/Alerm.vue
@@ -1,14 +1,78 @@
 <template>
-  <div class="alerm"></div>
+  <div class="alerm" v-bind:class="{ animation: isAnimation }">
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+    <p>ㅇㅇㅇㅇ</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isAnimation: true,
+    };
+  },
+};
 </script>
 
 <style>
 .alerm {
+  position: fixed;
+  top: 75px;
+  right: 0;
+  float: right;
   width: 300px;
-  background: #fff;
+  height: 500px;
+  z-index: 10;
+  overflow: auto;
+  background: red;
+}
+
+.animation {
+  animation: fadeInDown 1s;
+}
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    transform: translate3d(0, -100%, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translateZ(0);
+  }
 }
 </style>
